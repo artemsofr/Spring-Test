98f052eb259784763c174086fff45c90f1b369c4
Artem Sofronov <artemsofr1985@gmail.com>
2020-05-08 21:08:17 +0300

    Inversion of Control (with Injection with setters and value)Example.

--- spring-app1/src/main/java/ru/sofronov/springtest/MusicPlayer.java
+++ spring-app1/src/main/java/ru/sofronov/springtest/MusicPlayer.java
@@ -2,7 +2,8 @@ package ru.sofronov.springtest;
 
 public class MusicPlayer {
 	private Music music;
-	
+	private String name;
+	private int volume;
 	
 	public MusicPlayer(Music music) {
 		
@@ -15,10 +16,19 @@ public class MusicPlayer {
 	public void setMusic(Music music) {
 		this.music = music;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
 	public  void playMusic() {
 		
 		String song = music.getSong();
-		System.out.println("Playing song " + song);
+		System.out.println("Playing song " + song + ".");
+		System.out.println(name + " likes this song. He turns up volume to " + volume + ".");
 	}
-
+		
 }

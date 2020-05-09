7074d9a15e70d434757e3d0f68fafa43aecd7d98
Artem Sofronov <artemsofr1985@gmail.com>
2020-05-09 17:28:43 +0300

    init- and destroy-  bean methods (scope=singleton).

--- spring-app1/src/main/java/ru/sofronov/springtest/ClassicalMusic.java
+++ spring-app1/src/main/java/ru/sofronov/springtest/ClassicalMusic.java
@@ -2,9 +2,19 @@ package ru.sofronov.springtest;
 
 public class ClassicalMusic implements Music {
 	
+	public void doMyInit() {
+		
+		System.out.println("Doing my initialization.");
+	}
+	
+public void doMyDestroy() {
+		
+		System.out.println("Doing my destruction.");
+	}
+	
 	public String getSong() {
 		// TODO Auto-generated method stub
 		return "Peer Gunt Suite - Morning(Grieg)";
 	}
-
+	
 }
--- spring-app1/src/main/java/ru/sofronov/springtest/TestSpring.java
+++ spring-app1/src/main/java/ru/sofronov/springtest/TestSpring.java
@@ -9,31 +9,35 @@ public class TestSpring {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-		MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-		MusicPlayer thirdMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+		ClassicalMusic classicalMusic = context.getBean("musicBean1", ClassicalMusic.class);
+		
+		System.out.println(classicalMusic.getSong());
+		
+//		MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//		MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//		MusicPlayer thirdMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 		
 		//musicPlayer.playMusicList();
 		
-		firstMusicPlayer.playMusic();
-		
-		boolean comparison = firstMusicPlayer == secondMusicPlayer;
-		boolean comparison1 = firstMusicPlayer == thirdMusicPlayer;
-		boolean comparison2 = secondMusicPlayer == thirdMusicPlayer;
-		
-		System.out.println(comparison);
-		System.out.println(comparison1);
-		System.out.println(comparison2);
-		System.out.println(firstMusicPlayer);
-		System.out.println(secondMusicPlayer);
-		System.out.println(thirdMusicPlayer);
-		
-		
-		RockMusic rm = new RockMusic();
-		firstMusicPlayer.setMusic(rm);
-		firstMusicPlayer.playMusic();
-				
-		secondMusicPlayer.playMusic();
+//		firstMusicPlayer.playMusic();
+//		
+//		boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//		boolean comparison1 = firstMusicPlayer == thirdMusicPlayer;
+//		boolean comparison2 = secondMusicPlayer == thirdMusicPlayer;
+//		
+//		System.out.println(comparison);
+//		System.out.println(comparison1);
+//		System.out.println(comparison2);
+//		System.out.println(firstMusicPlayer);
+//		System.out.println(secondMusicPlayer);
+//		System.out.println(thirdMusicPlayer);
+//		
+//		
+//		RockMusic rm = new RockMusic();
+//		firstMusicPlayer.setMusic(rm);
+//		firstMusicPlayer.playMusic();
+//				
+//		secondMusicPlayer.playMusic();
 				
 		context.close();
 	}
